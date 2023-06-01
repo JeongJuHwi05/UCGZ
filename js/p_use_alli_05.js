2a7eab48975f3a97c03032c5c41b8d593ab70990
twinklebyeol <michele08@naver.com>
2023-06-01 09:16:13 +0900

    20230601 p_use 수정/김은별

--- js/p_use_alli_05.js
+++ js/p_use_alli_05.js
@@ -25,3 +25,11 @@ $(function () {
     }
   });
 });
+
+function openWindow1() {
+  window.open("/t_hotel/t_hotel_reser01.html", "_blank");
+}
+
+$(document).ready(function () {
+  $(".galleryborder").addClass("animate");
+});
